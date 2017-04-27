d96424fa416275c3e4586696e3041938693aa8f5
BY <by@BYdeiMac.local>
2017-04-27 14:44:23 +0800

    4-27-2

--- routes/code.js
+++ routes/code.js
@@ -5,10 +5,7 @@ var host="localhost";
 var port="27017";
 var birthCode=function (callback) {
 	var code="";	
-	var arr=['0','1','2','3','4','5','6','7','8','9',
-		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-	];
+	var arr=['0','1','2','3','4','5','6','7','8','9'];
 
 	var date=new Date();
 	var today=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
