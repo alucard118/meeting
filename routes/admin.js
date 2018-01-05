cca55664f01c579e0856d38a60d946d2fbcc6376
BY <alucard_118@163.com>
2018-01-05 22:55:21 +0800

    1-5-2

--- routes/admin.js
+++ routes/admin.js
@@ -23,8 +23,8 @@ router.get('/',function (req,res) {
 router.get('/staff',function (req,res) {
 	if(req.session.role=='superAdmin'){
 		//console.log(__dirname);
-		fs.readFile('./config/address.conf.js','utf-8',function (err,files) {
-			console.log(files.split(','));
+		fs.readFile('./config/address.conf','utf-8',function (err,files) {
+			//console.log(files.split(','));
 			res.render('./admin/admin_ccfStaff',{user:req.session.user,staff:files.split(',')});
 		})
 		
